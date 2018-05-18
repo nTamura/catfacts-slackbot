ea30b0d71553d2052d45189054cf1364d879082d
nTamura <nictamura@gmail.com>
2018-05-17 18:02:08 -0700

    switched to npm facts

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
+'use strict';
+
 require('dotenv').config()
 
 const Bot = require('slackbots')
+const catFacts = require('cat-facts');
 
 const settings = {
   token: process.env.TOKEN,
   name: 'Catfacts'
 }
 
-console.log(settings);
+let randomFact = catFacts.random();
+let allFacts = catFacts.all;
+
+// console.log(randomFact);
+// console.log(allFacts);
+
+const bot = new Bot(settings)
+
+bot.on('start', function() {
+  const params = {
+    icon_emoji: ':cf:'
+  };
+
+    // console.log(bot.getUsers())
+    // bot.postMessageToUser('ntamura', randomFact, params);
+    // bot.postMessageToChannel('general', 'meow!', params);
+    // bot.postMessageToGroup('testing', randomFact, params);
+})
+
+// bot.on('message', function(data) {
+//     // all ingoing events https://api.slack.com/rtm
+//     console.log(data);
+// });
