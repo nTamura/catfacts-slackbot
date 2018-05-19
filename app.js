e05d43066e8c99ad2ab183ec25e1bfba9aed8bc0
nTamura <nictamura@gmail.com>
2018-05-19 15:11:22 -0700

    before switch rtm api client

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 require('dotenv').config()
 
@@ -9,27 +9,41 @@ const settings = {
   token: process.env.TOKEN,
   name: 'Catfacts'
 }
+const params = {
+    icon_emoji: ':cf:'
+  };
 
 let randomFact = catFacts.random();
 let allFacts = catFacts.all;
 
-// console.log(randomFact);
-// console.log(allFacts);
-
 const bot = new Bot(settings)
 
-bot.on('start', function() {
-  const params = {
-    icon_emoji: ':cf:'
-  };
+// bot.on('start', function() {
+//   console.log('Catfacts server running...');
+//   // console.log(bot.getUsers())
+//   // bot.postMessageToUser('ntamura', randomFact, params);
+//   // bot.postMessageToChannel('', randomFact, params);
+//   // bot.postMessageToGroup('testing', randomFact, params);
+// })
 
-    // console.log(bot.getUsers())
-    // bot.postMessageToUser('ntamura', randomFact, params);
-    // bot.postMessageToChannel('general', 'meow!', params);
-    // bot.postMessageToGroup('testing', randomFact, params);
-})
+bot.on('message', function(data) {
+  if (data.type === "desktop_notification") {
+    let text = data.content
+    let arg = /([\/])\w+/.exec(text);
 
-// bot.on('message', function(data) {
-//     // all ingoing events https://api.slack.com/rtm
-//     console.log(data);
-// });
+    if (arg[0]) {
+      switch(arg[0]) {
+        case "/facts":
+          // console.log('/fact!');
+          bot.postMessageToGroup('testing', randomFact, params);
+          break;
+        default:
+          console.log('default');
+      }
+    } else {
+      console.log('error');
+    }
+
+  }
+
+});
